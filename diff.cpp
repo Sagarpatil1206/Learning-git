c3764c2df8e76465168abaf9340b347e2bf55a92
Sagar Patil <sagarpatil20601@gmail.com>
2022-06-05 00:24:25 +0530

    change in div

--- diff.cpp
+++ diff.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-  int a,b;
-  cout<<"ENTER a AND b :"<<endl;
-  cin>>a>>b;
-  cout<<"DIFFERENCE OF a AND b is :"<<(a-b)<<endl;
+  int x,y;
+  cout<<"ENTER a AND b : "<<endl;
+  cin>>x>>y;
+  cout<<"DIFFERENCE OF a AND b is :"<<(x-y)<<endl;
 }
